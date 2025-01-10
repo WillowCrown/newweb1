be18c02988189b7d0a4f5d59acd14b20fbf91423
Pinya2403 <xuanphu365@gmail.com>
2025-01-10 15:57:30 +0700

    16.1

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
     has_rich_text :description
   validates :name, presence: true
   has_one_attached :featured_image
+  validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_015712) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_085346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_015712) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "inventory_count"
   end
 
   create_table "users", force: :cascade do |t|
